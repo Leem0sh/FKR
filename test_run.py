c49fe560d19dc8da48c0d0a80c6112d8dafd3fa5
Šimon Líman <simon.liman@biano.com>
2021-12-09 02:17:32 +0100

    reqs up, test run localhost

--- test_run.py
+++ test_run.py
@@ -6,6 +6,6 @@ import requests
 data = {'description': 'random description'}
 
 
-for x in range(600):
-    r = requests.post('http://127.0.0.1:8000/dimensions/measure/1/nabytek', json=data)
+for x in range(1000):
+    r = requests.post('http://127.0.0.1:8000/test/', json=data)
     print(r.text)
